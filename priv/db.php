6d44a81163c88cec240cce904a1d495e0b3e2be8
Lv Zhaobo <lvzhaobo@gmail.com>
2014-05-21 01:06:19 +0800

    编辑用户时增加用户权限选项

--- priv/db.php
+++ priv/db.php
@@ -28,7 +28,7 @@ class my_user
 	public function getUserByName($username){
 		$result = mysql_query("select * from user where User='".$username."';");
 		$data = mysql_fetch_assoc($result);
-		//var_dump($data);
+		var_dump($data);
 		return $data;
 	}
 	
@@ -59,13 +59,22 @@ class my_user
 		$username = $_POST["username"];
 		$password = $_POST["password"];
 		$priv = join($_POST["auth"],",");
+		
+		$sql_update_item = array();
+		foreach($_POST["auth"] as $item){
+			$sql_update_item[] = $item."='Y'";
+		}
+		$sql_update = "UPDATE mysql.user SET ".join(",",$sql_update_item)." WHERE User='".$username."';";
+		var_dump($sql_update);//die;
 		$sql = "REVOKE ALL ON *.* FROM '".$username."'@'%'";
 		$sql1 = "GRANT ".$priv." ON user.* TO '".$username."'@'%' IDENTIFIED BY '".$password."' WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0 ;";
 		mysql_query($sql);
+		mysql_query($sql_update);
+		
 		mysql_query($sql1);
 		//var_dump(mysql_error(),$sql,$sql1);
 		
-		$user->close();
+		//$user->close();
 		redirect();
 	}
 	
--- priv/edituser.php
+++ priv/edituser.php
@@ -23,14 +23,24 @@
 	  <tr><th>用户名</th><td><input name="username" value="<?php echo $data["User"]?>"/></td></tr>
 	  <tr><th>密码</th><td><input name="password" type="password" /></td></tr>
 	  <tr><th>权限</th><td>
-	    <input type="checkbox" name="auth[1]" id="auth_1" value="SELECT" <?php echo $data["Select_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_1">SELECT</label><br />
-		<input type="checkbox" name="auth[2]" id="auth_2" value="INSERT" <?php echo $data["Insert_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_2">INSERT</label><br />
-		<input type="checkbox" name="auth[3]" id="auth_3" value="UPDATE" <?php echo $data["Update_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_3">UPDATE</label><br />
-		<input type="checkbox" name="auth[4]" id="auth_4" value="DELETE" <?php echo $data["Delete_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_4">DELETE</label><br />
+	    <fieldset style="float:left;">
+	      <input type="checkbox" name="auth['Select_priv']" id="auth_1" value="Select_priv" <?php echo $data["Select_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_1">SELECT</label><br />
+		  <input type="checkbox" name="auth['Insert_priv']" id="auth_2" value="Insert_priv" <?php echo $data["Insert_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_2">INSERT</label><br />
+		  <input type="checkbox" name="auth['Update_priv']" id="auth_3" value="Update_priv" <?php echo $data["Update_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_3">UPDATE</label><br />
+		  <input type="checkbox" name="auth['Delete_priv']" id="auth_4" value="Delete_priv" <?php echo $data["Delete_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_4">DELETE</label><br />
+		  <input type="checkbox" name="auth['File_priv']" id="auth_5" value="File_priv" <?php echo $data["File_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_5">FILE</label><br />
+		</fieldset>
+		<fieldset style="float:left;">
+	      <input type="checkbox" name="auth['Create_priv']" id="auth_6" value="Create_priv" <?php echo $data["Create_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_6">CREATE</label><br />
+		  <input type="checkbox" name="auth['Alter_priv']" id="auth_7" value="Alter_priv" <?php echo $data["Alter_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_7">ALTER</label><br />
+		  <input type="checkbox" name="auth['Index_priv']" id="auth_8" value="Index_priv" <?php echo $data["Index_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_8">INDEX</label><br />
+		  <input type="checkbox" name="auth['Drop_priv']" id="auth_9" value="Drop_priv" <?php echo $data["Drop_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_9">DROP</label><br />
+		  <input type="checkbox" name="auth['Create_tmp_table_priv']" id="auth_10" value="Create_tmp_table_priv" <?php echo $data["Create_tmp_table_priv"]=="Y"?"checked='checked'":""?>/><label for="auth_10">CREATE TEMPORARY TABLES</label><br />
+		</fieldset>
 	  </td></tr>
 	  <tr><th></th><td></td></tr>
 	</table>
-	<button type="submit">确定</button><a href="index.php">取消</a>
+	<button type="submit">确定</button><a href="index.php">取消</a>	
 	</form>
   </div>
 	</fieldset>
