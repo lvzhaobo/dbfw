3168112ad62035d5c67114d523bce535d53de421
Lv Zhaobo <lvzhaobo@gmail.com>
2014-06-27 17:44:26 +0800

    update

--- db_src/test.py
+++ db_src/test.py
@@ -58,10 +58,34 @@ def main():
 
     cursor.close()
     conn.close()
-class mysql_db:
+class mysql_db(object):
     def test(self):
         print "test"
     
+    def get_user(self):
+        try:
+            conn = mysql.connector.connect(host='localhost', user='root',passwd='111111',db='mysql')
+        except Exception, e:
+            print e
+        
+        cursor = conn.cursor()
+		
+        sql = "select * from user"
+        cursor.execute(sql)
+        alldata = cursor.fetchall()
+        
+        if alldata:
+            data = []
+            for rec in alldata:
+                print rec[0]+"\t"+rec[1]
+                data.append(rec[0]+"\t"+rec[1])
+            file_object = open('data.txt', 'w+')
+            file_object.write("\n".join(data))
+            file_object.close( )
+
+        cursor.close()
+        conn.close()
+        
 def adduser():
     #a = mysql_db()
     #a.test()
@@ -69,10 +93,13 @@ def adduser():
     
 if __name__ == "__main__":
     opts, args = getopt.getopt(sys.argv[1:], "ha:")
-	
     if(args[0] == "adduser"):
         a = mysql_db()
-        a.test()
+        a.adduser()
+        
+    if args[0]=="list":
+        a = mysql_db()
+        a.get_user()
 
 
 #print '{"page":1,"total":239,"rows":[{"id":"ZW","cell":{"name":"Zimbabwe ","iso":"ZW","printable_name":"Zimbabwe ","iso3":"ZWE ","numcode":"716"}},{"id":"ZM","cell":{"name":"Zambia ","iso":"ZM","printable_name":"Zambia ","iso3":"ZMB ","numcode":"894"}},{"id":"YE","cell":{"name":"Yemen ","iso":"YE","printable_name":"Yemen ","iso3":"YEM ","numcode":"887"}},{"id":"EH","cell":{"name":"Western Sahara ","iso":"EH","printable_name":"Western Sahara ","iso3":"ESH ","numcode":"732"}},{"id":"WF","cell":{"name":"Wallis and Futuna ","iso":"WF","printable_name":"Wallis and Futuna ","iso3":"WLF ","numcode":"876"}},{"id":"VI","cell":{"name":"Virgin Islands, U.s. ","iso":"VI","printable_name":"Virgin Islands, U.s. ","iso3":"VIR ","numcode":"850"}},{"id":"VG","cell":{"name":"Virgin Islands, British ","iso":"VG","printable_name":"Virgin Islands, British ","iso3":"VGB ","numcode":"92"}},{"id":"VN","cell":{"name":"Viet Nam ","iso":"VN","printable_name":"Viet Nam ","iso3":"VNM ","numcode":"704"}},{"id":"VE","cell":{"name":"Venezuela ","iso":"VE","printable_name":"Venezuela ","iso3":"VEN ","numcode":"862"}},{"id":"VU","cell":{"name":"Vanuatu ","iso":"VU","printable_name":"Vanuatu ","iso3":"VUT ","numcode":"548"}}],"post":[]}'
--- priv/index.php
+++ priv/index.php
@@ -2,127 +2,127 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <link rel="stylesheet" type="text/css" href="../src/main.css" />
-	<title>数据库防火墙--DBFW</title>
-	<style type="text/css">
-	  table td {
-		border: 0.1em solid #000000;
-	  }
-	  table {
-	    border-collapse: collapse;
-	  }
-	</style>
-	<link rel="stylesheet" href="../web/flexigrid-1.1/css/flexigrid.css" type="text/css"></link>  
+    <title>数据库防火墙--DBFW</title>
+    <style type="text/css">
+      table td {
+        border: 0.1em solid #000000;
+      }
+      table {
+        border-collapse: collapse;
+      }
+    </style>
+    <link rel="stylesheet" href="../web/flexigrid-1.1/css/flexigrid.css" type="text/css"></link>  
 <script src="//code.jquery.com/jquery-1.8.0.min.js"></script>
 <script type="text/javascript" src="../web/flexigrid-1.1/js/flexigrid.pack.js"></script>  
   </head>
   <body>
     <?php include '../web/_header.php'?>
-	<?php include "../web/_nav.php"?>
-	<div id="content">
-	  <div id="main" style="padding:10px 0;">
-		<p>
-		  1.可以控制对数据库、表、列等的控制
-		  2.可以根据主机IP、时间等进行控制
-		  3.可以对用户赋予（GRANT）或取消（REVOKE）权限
-		  4.基于角色的权限控制
-		</p>
-		
-		<div id="flex1" class="flex1" style="width:100%;height:100%;"></div>
+    <?php include "../web/_nav.php"?>
+    <div id="content">
+      <div id="main" style="padding:10px 0;">
+        <p>
+          1.可以控制对数据库、表、列等的控制
+          2.可以根据主机IP、时间等进行控制
+          3.可以对用户赋予（GRANT）或取消（REVOKE）权限
+          4.基于角色的权限控制
+        </p>
+        
+        <div id="flex1" class="flex1" style="width:100%;height:100%;"></div>
 <script type="text/javascript">
 
 $("#flex1").flexigrid({
-	url: '../tmp/a.php',
-	dataType: 'json',
-	colModel : [
-		{display: '', name : 'id', width : 40, sortable : true, align: 'left'},
-		{display: '主机', name : 'host', width : 180, sortable : true, align: 'left'},
-		{display: '用户名', name : 'username', width : 180, sortable : true, align: 'left'},
-		{display: '权限', name : 'priv', width : 120, sortable : true, align: 'left'}
-		],
-	buttons : [
-		{name: '添加', bclass: 'add', onpress : test},
-		{name: '编辑', bclass: 'edit', onpress : test},
-		{name: '删除', bclass: 'delete', onpress : test},
-		{separator: true}
-		],
-	searchitems : [
-		{display: 'ISO', name : 'host'},
-		{display: 'Name', name : 'username', isdefault: true}
-		],
-	sortname: "iso",
-	sortorder: "asc",
-	usepager: true,
-	title: 'Countries',
-	useRp: true,
-	rp: 15,
-	showTableToggleBtn: true,
-	width: 700,
-	height: 200
+    url: '../db_src/a.php',
+    dataType: 'json',
+    colModel : [
+        {display: '', name : 'id', width : 40, sortable : true, align: 'left'},
+        {display: '主机', name : 'host', width : 180, sortable : true, align: 'left'},
+        {display: '用户名', name : 'username', width : 180, sortable : true, align: 'left'},
+        {display: '权限', name : 'priv', width : 120, sortable : true, align: 'left'}
+        ],
+    buttons : [
+        {name: '添加', bclass: 'add', onpress : test},
+        {name: '编辑', bclass: 'edit', onpress : test},
+        {name: '删除', bclass: 'delete', onpress : test},
+        {separator: true}
+        ],
+    searchitems : [
+        {display: 'ISO', name : 'host'},
+        {display: 'Name', name : 'username', isdefault: true}
+        ],
+    sortname: "iso",
+    sortorder: "asc",
+    usepager: true,
+    title: 'Countries',
+    useRp: true,
+    rp: 15,
+    showTableToggleBtn: true,
+    width: 700,
+    height: 200
 });
 function test(){
-	location = "adduser.php";
+    location = "adduser.php";
 }
 
 </script>
 
-		<?php
-			try{
-				exec("python2.7 ../test1.py");
-			}catch(Exception $e){
-				var_dump($e->getMessage());
-			}
+        <?php
+            try{
+                exec("python2.7 ../test1.py");
+            }catch(Exception $e){
+                var_dump($e->getMessage());
+            }
 
-			$data = file_get_contents("../priv.txt");
-	    	$data_array = explode("\n",$data);
-	    	
-			/*$conn = mysql_connect("localhost","root","");
-			mysql_select_db("mysql");
-			$result = mysql_query("select * from user;");
-			$data = array();
-			while($row = mysql_fetch_array($result)){
-				//var_dump($row);
-				//echo "<hr />";
-				$data[] = array('host'=>$row["Host"],'user'=>$row["User"],'password'=>$row["Password"]);
-			}*/
-			
-		?>
-		<div style="height:24px;padding:5px 2%;">
-		  <div style="float:left;">
-		    <span style="font-weight:bold;">用户权限</span>
-		  </div>
-		  <div style="float:right;">
-		    <a href="adduser.php">添加用户</a>
-		  </div>
-		</div>
-		<table style="margin:0 2% 10px 2%;width:96%;">
-		  <thead style="background-color:#E5E5E5;">
-	        <tr><td width="140px;">用户</td><td width="200px;">主机</td><td width="280px;">权限</td><td width="100px;">操作</td></tr>
-		  </thead>
-		  <tbody>
-			<?php
-				foreach($data_array as $item_v){
-					$item = explode("\t",$item_v);
-					$user_host = "'".$item[0]."'@'".$item[1]."'";
-					if(empty($item[0]))
-						$item[0] = "任意";
-					//var_dump($user_host);
-					//$grant = mysql_query("show grants for ".$user_host.";");
-					//if(!$grant)
-						//var_dump("show grants for ".$user_host.";");echo "-----------------------------";
-					$tmp_grant_str = "";
-					//while($row = mysql_fetch_array($grant)){
-						//$tmp_grant_str .= $row[0]."<br />";
-					//}
-					$tmp_grant_str = "";
-					echo "<tr><td>".$item[0]."</td><td>".$item[1]."</td><td>".$tmp_grant_str."</td><td><a href='edituser.php?user=".$item[0]."'>编辑</a></td></tr>";
-				}
-			?>
-			</tbody>
-		  </table>
-		</div>
-	  <div class="clearfloat"></div>
-	</div>
-	<?php include "../web/_footer.php"?>
-	<div class="clearfloat"></div>
+            $data = file_get_contents("../priv.txt");
+            $data_array = explode("\n",$data);
+            
+            /*$conn = mysql_connect("localhost","root","");
+            mysql_select_db("mysql");
+            $result = mysql_query("select * from user;");
+            $data = array();
+            while($row = mysql_fetch_array($result)){
+                //var_dump($row);
+                //echo "<hr />";
+                $data[] = array('host'=>$row["Host"],'user'=>$row["User"],'password'=>$row["Password"]);
+            }*/
+            
+        ?>
+        <div style="height:24px;padding:5px 2%;">
+          <div style="float:left;">
+            <span style="font-weight:bold;">用户权限</span>
+          </div>
+          <div style="float:right;">
+            <a href="adduser.php">添加用户</a>
+          </div>
+        </div>
+        <table style="margin:0 2% 10px 2%;width:96%;">
+          <thead style="background-color:#E5E5E5;">
+            <tr><td width="140px;">用户</td><td width="200px;">主机</td><td width="280px;">权限</td><td width="100px;">操作</td></tr>
+          </thead>
+          <tbody>
+            <?php
+                foreach($data_array as $item_v){
+                    $item = explode("\t",$item_v);
+                    $user_host = "'".$item[0]."'@'".$item[1]."'";
+                    if(empty($item[0]))
+                        $item[0] = "任意";
+                    //var_dump($user_host);
+                    //$grant = mysql_query("show grants for ".$user_host.";");
+                    //if(!$grant)
+                        //var_dump("show grants for ".$user_host.";");echo "-----------------------------";
+                    $tmp_grant_str = "";
+                    //while($row = mysql_fetch_array($grant)){
+                        //$tmp_grant_str .= $row[0]."<br />";
+                    //}
+                    $tmp_grant_str = "";
+                    echo "<tr><td>".$item[0]."</td><td>".$item[1]."</td><td>".$tmp_grant_str."</td><td><a href='edituser.php?user=".$item[0]."'>编辑</a></td></tr>";
+                }
+            ?>
+            </tbody>
+          </table>
+        </div>
+      <div class="clearfloat"></div>
+    </div>
+    <?php include "../web/_footer.php"?>
+    <div class="clearfloat"></div>
   </body>
 </html>
